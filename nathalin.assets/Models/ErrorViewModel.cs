0ec8573e640a19e02baf9181c4795888b3d442a2
Yongyut Pudwong <yongyut@nathalin.com>
2020-12-02 14:46:29 +0700

    Add project files.

---
+++ nathalin.assets/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nathalin.assets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
